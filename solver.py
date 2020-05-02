c5179c3766dd418ca6547145f45514d26e9e730c
Ashwin Rastogi <ashwinrastogi@berkeley.edu>
2020-05-02 14:24:57 -0500

    Before making into digraph

--- solver.py
+++ solver.py
@@ -7,7 +7,7 @@ from mip import Model, xsum, minimize, BINARY
 import sys
 import glob
 
-DEFAULT_MAX_WEIGHT = 100000000.0
+DEFAULT_MAX_WEIGHT = float(2 ** 128)
 
 def solve(G):
     """
@@ -34,7 +34,7 @@ def solve(G):
         if y[i].x >= 0.99:
             T.add_node(i)
     
-    # print([y[i].x for i in range(len(y))])
+    print([y[i].x for i in range(len(y))])
 
     # Added included edges in model
     for i in V:
@@ -48,8 +48,10 @@ def solve(G):
     # print("Number of edges: ", T.number_of_edges())
     # print("Number of vertices: ", T.number_of_nodes())
 
-    # nx.draw(T, with_labels=True, font_weight='bold')
-    # plt.show()
+    nx.draw(T, with_labels=True, font_weight='bold')
+    plt.show()
+
+    print("Is dominating: ", nx.is_dominating_set(G, T.nodes))
 
     # print("Average pairwise distance: ", average_pairwise_distance(T))
 
@@ -62,13 +64,13 @@ def solve(G):
 
 
 
-def make_model(G: nx.Graph):
+def make_model(G_copy: nx.Graph):
     """
     Args:
         G: networkx.Graph
     """
 
-    dist = [[DEFAULT_MAX_WEIGHT] * G.number_of_nodes()] * G.number_of_nodes()
+    G = nx.Graph(G_copy)
     n, V = G.number_of_nodes(), set(range(G.number_of_nodes()))
     
     # Fill in edge weights into adjacency matrix
@@ -77,6 +79,23 @@ def make_model(G: nx.Graph):
             if not G.has_edge(i, j):
                 G.add_edge(i, j)
                 G[i][j]['weight'] = DEFAULT_MAX_WEIGHT
+            G[i][j]['capacity'] = int(i != j)
+
+    # Make supersource and supersink
+    source = n + 1
+    sink = n + 2
+
+    G.add_node(source)
+    G.add_node(sink)
+
+    for i in V:
+        G.add_edge(sink, i)
+        G[i][sink]['capacity'] = 1.0
+        G[i][sink]['weight'] = DEFAULT_MAX_WEIGHT
+    
+        G.add_edge(source, i)
+        G[i][source]['capacity'] = float(n)
+        G[i][source]['weight'] = DEFAULT_MAX_WEIGHT
 
     model = Model()
     model.emphasis = 2
@@ -85,11 +104,24 @@ def make_model(G: nx.Graph):
     x = [[model.add_var(var_type=BINARY) for j in V] for i in V]
     # Making boolean variables for each vertex
     y = [model.add_var(var_type=BINARY) for i in V]
-
+    # Making boolean variables for logical OR
     z = [model.add_var(var_type=BINARY) for i in V]
+    # Making total flow variables
+    x_source = [model.add_var(var_type=BINARY) for i in V]
+    x_sink = [model.add_var(var_type=BINARY) for i in V]
+    flow = [[model.add_var() for j in V] for i in V]
 
     # Objective function: minimize pairwise distance
     model.objective = minimize(xsum(x[i][j] * G[i][j]['weight'] for i in V for j in V))
+
+    # Constraint: Total flow with only one source connected vertex
+    model += xsum(x_source[i] for i in V) == 1
+
+    # Constraint: flow must be non negative and <= capacity
+    for i in V:
+        for j in V:
+            model += flow[i][j] <= G[i][j]['capacity']
+            model += flow[i][j] >= 0
 
     # Constraint: at least one vertex must be chosen
     model += xsum(y[i] for i in V) >= 1
@@ -110,15 +142,20 @@ def make_model(G: nx.Graph):
     for i in V:
         model += y[i] <= 1
         neighbors = G.adj[i]
-        num_neighbors = len(neighbors)
+        num_neighbors = len([k for k in neighbors if G[i][k]['weight'] <= 100])
 
-        model += num_neighbors - xsum(x[i][j] for j in neighbors) >= num_neighbors * (1 - z[i])
-        model += num_neighbors * (1 - z[i]) >= 1 - xsum(x[i][j] for j in V)
+        # model += num_neighbors - xsum(x[i][j] for j in neighbors) >= num_neighbors * (1 - z[i])
+        # model += num_neighbors * (1 - z[i]) >= 1 - xsum(x[i][j] for j in V)
 
-        model += y[i] <= z[i]
+        model += xsum(y[j] for j in neighbors if j != i and G[i][j]['weight'] <= 100) <= num_neighbors * z[i]
+        model += num_neighbors * z[i] <= xsum(y[j] for j in V if j != i and G[i][j]['weight'] <= 100) + num_neighbors - 1
+
+        model += y[i] >= 1 - z[i]
+        model += xsum(x[i][j] for j in neighbors if G[i][j]['weight'] <= 100) >= y[i]
     
     # Constraint: number of edges is number of vertices - 1 (Tree)
-    model += 0.5 * xsum(x[i][j] for i in V for j in V) == xsum(y[i] for i in V) - 1
+    model += 0.5 * xsum(x[i][j] for i in V for j in V if i != j) == xsum(y[i] for i in V) - 1
+
     return model, x, y, V
 
 def check_answer():
@@ -140,15 +177,16 @@ def check_answer():
 
 # Usage: python3 solver.py test.in
 if __name__ == '__main__':
-    # assert len(sys.argv) == 2
-    # path = sys.argv[1]
-    # G = read_input_file(path)
-    # T = solve(G)
-    # assert is_valid_network(G, T)
-    # print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-    # write_output_file(T, 'out/test_medium.out')
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G = read_input_file(path)
+    T = solve(G)
+    assert is_valid_network(G, T)
+    print('GOT THRU BB')
+    print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+    write_output_file(T, 'out/test_medium.out')
 
-    check_answer()
+    #check_answer()
 
     # unsolved = []
 
