82eed5170ee6e381dc863e09fd3288f2c10d9016
Ashwin Rastogi <ashwinrastogi@berkeley.edu>
2020-05-03 20:55:29 -0500

    Cleaned up solver

--- max_st.py
+++ max_st.py
@@ -1,6 +1,6 @@
 from parse import *
 import networkx as nx
-from solver import greedy_solve
+from solver import solve
 import os
 
 if __name__ == "__main__":
@@ -10,5 +10,5 @@ if __name__ == "__main__":
         print(input_path)
         graph_name = input_path.split(".")[0]
         G = read_input_file(f"{input_dir}/{input_path}")
-        T = greedy_solve(G)
+        T = solve(G)
         write_output_file(T, f"{output_dir}/{graph_name}.out")
--- solver.py
+++ solver.py
@@ -18,53 +18,8 @@ def solve(G):
     Returns:
         T: networkx.Graph
     """
-    
-    model, x, y, V = make_model_new(G)
-    model.optimize(max_seconds=60)
-    
-    if not model.num_solutions:
-        print("Objective value: ", model.objective_value)
-        print("No solutions found")
-        return G
-    
-    print("Solution found with objective value: ", model.objective_value)
-    T = nx.Graph()
 
-    # Added included vertices in model
-    for i in V:
-        if y[i].x >= 0.99:
-            T.add_node(i)
-    
-    print([y[i].x for i in range(len(y))])
-
-    # Added included edges in model
-    for i in V:
-        for j in V:
-            if i != j and x[i][j].x >= 0.99 and G[i][j]['weight'] <= 100:
-                T.add_edge(i, j)
-
-                T[i][j]['weight'] = G[i][j]['weight']
-                #print(i, j, dist[i][j])
-
-    # print("Number of edges: ", T.number_of_edges())
-    # print("Number of vertices: ", T.number_of_nodes())
-
-    #new_T = get_mst_T(G, T)
-
-    nx.draw(T, with_labels=True, font_weight='bold') # make sure to change new_T back to T
-    plt.show()
-
-    print("Is dominating: ", nx.is_dominating_set(G, T.nodes)) # same here ^^
-
-    # print("Average pairwise distance: ", average_pairwise_distance(T))
-
-   # if is_valid_network(G, T):
-        # print("Average pairwise distance: ", average_pairwise_distance(T))
-       # return T
-
-    # print("Found solution but is not valid tree")
-
-    return T # change this back to T
+    return greedy_solve(G)
 
 def greedy_solve(G):
     """
@@ -156,8 +111,6 @@ def removal_cost(cur_cost, node_to_remove, attach_node, T, dist_to_attach, dist)
     cost = cur_cost * (num_nodes)/(num_nodes-2) - 2 * new_sum / ((num_nodes-1)*(num_nodes-2))
     
     return cost
-    
-
 
 def get_mst_T(G, T):
     new_T = nx.Graph(G)
@@ -168,93 +121,6 @@ def get_mst_T(G, T):
             new_T.remove_node(v)
 
     return nx.minimum_spanning_tree(new_T)
-
-def make_model(G_copy: nx.Graph):
-    """
-    Args:
-        G: networkx.Graph
-    """
-
-    G = nx.Graph(G_copy)
-    n, V = G.number_of_nodes(), set(range(G.number_of_nodes()))
-    super_node = n
-    
-    # Fill in edge weights into adjacency matrix
-    for i in V:
-        for j in V:
-            if not G.has_edge(i, j):
-                G.add_edge(i, j)
-                G[i][j]['weight'] = DEFAULT_MAX_WEIGHT
-
-    G.add_node(super_node)
-    for i in V:
-        G.add_edge(super_node, i)
-        G[super_node][i]['weight'] = DEFAULT_MAX_WEIGHT
-
-    model = Model()
-    model.emphasis = 0
-
-    # Making boolean variables for each edge
-    x = [[model.add_var(var_type=BINARY) for j in V] for i in V]
-    # Making boolean variables for each vertex
-    y = [model.add_var(var_type=BINARY) for i in V]
-    # Making boolean variables for logical OR
-    z = [model.add_var(var_type=BINARY) for i in V]
-    # Making boolean variables for connectivity
-    c = [model.add_var(var_type=BINARY) for i in V]
-    c.append(model.add_var(var_type=BINARY))
-    chosen = [model.add_var(var_type=BINARY) for i in V]
-
-    # Objective function: minimize pairwise distance
-    model.objective = minimize(xsum(x[i][j] * G[i][j]['weight'] for i in V for j in V))
-
-    # Constraint: at least one vertex must be chosen
-    model += xsum(y[i] for i in V) >= 1
-
-    # Constraint: at most n vertices can be chosen
-    model += xsum(y[i] for i in V) <= n
-
-    # Constraint: edge is included only if both vertices included
-    for i in range(n):
-        for j in range(n):
-            model += x[i][j] <= y[i]
-            model += x[i][j] <= y[j]
-
-            model += x[i][j] == x[j][i]
-    
-    # Constraint: if neighbor is selected, can be 0 or 1
-    # For each vertex, y_i must 1 if none of its neighbors is 1
-    for i in V:
-        model += y[i] <= 1
-
-        model += xsum(y[j] for j in G.adj[i] if j != i and G[i][j]['weight'] <= 100) <= num(G, i) * z[i]
-        model += num(G, i) * z[i] <= xsum(y[j] for j in V if j != i and G[i][j]['weight'] <= 100) + num(G, i) - 1
-
-        model += y[i] >= 1 - z[i]
-        model += xsum(x[i][j] for j in G.adj[i] if G[i][j]['weight'] <= 100) >= y[i]
-    
-    # Constraint: number of edges is number of vertices - 1 (Tree)
-    model += 0.5 * xsum(x[i][j] for i in V for j in V if i != j) == xsum(y[i] for i in V) - 1
-
-    # Constraint: tree connected if all vertices are connected
-    model += c[super_node] >= 1
-    # Constraint: super node connected to only 1 vertex
-    model += xsum(chosen[i] for i in V) == 1
-
-    for i in V:
-        model += c[i] == chosen[i]
-
-    for i in V:
-        for j in G.adj[i]:
-            if G[i][j]['weight'] <= 100:
-                model += c[i] >= c[j] + x[i][j] - 1
-                model += c[i] <= c[j]
-                model += c[i] <= x[i][j]
-    
-    model += xsum(c[i] for i in V) == xsum(y[i] for i in V)
-
-    return model, x, y, V
-
 
 def make_model_new(G_init: nx.Graph):
     G = nx.Graph(G_init)
@@ -277,8 +143,7 @@ def make_model_new(G_init: nx.Graph):
     x = [[model.add_var(var_type=BINARY) for j in V] for i in V]
     
     # Objective: minimize pairwise distance
-    # model.objective = minimize(xsum(x[i][j] * G[i][j]['weight'] for i in V for j in V))
-    model.objective = minimize(xsum(y[i] for i in V))
+    model.objective = minimize(xsum(x[i][j] * G[i][j]['weight'] for i in V for j in V))
     # Constraint: at least one vertex must be chosen
     model += xsum(y[i] for i in V) >= 1
     # Constraint: at most n vertices can be chosen
@@ -302,6 +167,7 @@ def make_model_new(G_init: nx.Graph):
 
     return model, x, y, V
 
+# Adds all exponential connectivity constraints to model
 def connectivity_constraints(model: Model, V: int, subV, x, y, prev=-1):
     for i in range(prev + 1, V):
         subV.add(i)
@@ -310,6 +176,7 @@ def connectivity_constraints(model: Model, V: int, subV, x, y, prev=-1):
         connectivity_constraints(model, V, subV, x, y, prev=i)
         subV.remove(i)
 
+# Generates directed graph with forward edges removed
 def make_flow_graph(G: nx.Graph):
     n = G.number_of_nodes()
     V = set(range(n))
@@ -334,9 +201,7 @@ def make_flow_graph(G: nx.Graph):
 
     return F
 
-def num(G: nx.Graph, i: int):
-    return len([k for k in G.adj[i] if G[i][k]['weight'] <= 100])
-
+# Checks if outputs are correct
 def check_answer():
     incorrect = []
     for file in glob.glob('submission/medium-*.out'):
@@ -351,7 +216,7 @@ def check_answer():
     with open('medium_not_work.txt', 'w') as fo:
         fo.write("\n".join(incorrect))
         fo.close()
-    
+
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
@@ -359,26 +224,7 @@ if __name__ == '__main__':
     assert len(sys.argv) == 2
     path = sys.argv[1]
     G = read_input_file(path)
-
-    T = greedy_solve(G)
-
+    T = solve(G)
     assert is_valid_network(G, T)
-    print('GOT THRU BB')
     print("Average  pairwise distance: {}".format(average_pairwise_distance_fast(T)))
-    # write_output_file(T, 'out/test_medium.out')
-
-    #check_answer()
-
-    # unsolved = []
-
-    # for file in glob.glob('inputs/small-*.in'):
-    #     G = read_input_file(file)
-    #     T = solve(G)
-    #     if is_valid_network(G, T):
-    #         print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
-    #         write_output_file(T, 'out/' + file[7:-2] + 'out')
-    #     else:
-    #         print("DIDN'T WORK: ", file)
-    #         unsolved.append(file)
-    
-    # print(unsolved)
+    write_output_file(T, 'out/test.out')
